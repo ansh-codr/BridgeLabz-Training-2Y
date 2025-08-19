6f510f0490e0933a761ec7126a2011772640a737
Byte-Nash <bytenashrao@gmail.com>
2025-08-19 08:47:29 +0530

    Fundamental Practice Problem

---
+++ Fundamental-Practice-Problem/AreaofaCircle.java
@@ -0,0 +1,7 @@
+public class AreaofaCircle {
+    public static void main(String[] args) {
+        double radius = 5.0;
+        double area = Math.PI * radius * radius;
+        System.out.println("The area of a circle with radius " + radius + " is: " + area);
+    }
+}
