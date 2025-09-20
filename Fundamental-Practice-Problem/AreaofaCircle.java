656289b524e761d4e92a93d27c5f5e8164dfdf17
Ansh yadav <183394850+ansh-codr@users.noreply.github.com>
2025-09-20 10:55:23 +0530

    Remove Core Java folders from OOPS branch

--- Fundamental-Practice-Problem/AreaofaCircle.java
+++
@@ -1,7 +0,0 @@
-public class AreaofaCircle {
-    public static void main(String[] args) {
-        double radius = 5.0;
-        double area = Math.PI * radius * radius;
-        System.out.println("The area of a circle with radius " + radius + " is: " + area);
-    }
-}
--- Fundamental-Practice-Problem/CalculateSimpleInterest.java
+++
@@ -1,9 +0,0 @@
-public class CalculateSimpleInterest {
-    public static void main(String[] args) {
-        double principal = 1000.0;
-        double rate = 5.0;
-        double time = 3.0;
-        double interest = (principal * rate * time) / 100;
-        System.out.println("The simple interest is: " + interest);
-    }
-}
