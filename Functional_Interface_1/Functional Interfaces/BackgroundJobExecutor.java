286f7d2bf8012cc32b3b9c5e7bde76d82a13c414
Ansh yadav <183394850+ansh-codr@users.noreply.github.com>
2025-11-04 11:06:03 +0530

    Simplify Functional Interfaces examples (beginner-friendly, no lambdas/threads)

--- Functional_Interface_1/Functional Interfaces/BackgroundJobExecutor.java
+++ Functional_Interface_1/Functional Interfaces/BackgroundJobExecutor.java
@@ -1,30 +1,13 @@
 public class BackgroundJobExecutor {
-    
+    static void task(int n) {
+        System.out.println("Task " + n + " started");
+        System.out.println("Task " + n + " completed");
+    }
+
     public static void main(String[] args) {
-        
-        Runnable job1 = () -> {
-            System.out.println("Task 1 started");
-            System.out.println("Task 1 completed");
-        };
-        
-        Runnable job2 = () -> {
-            System.out.println("Task 2 started");
-            System.out.println("Task 2 completed");
-        };
-        
-        Runnable job3 = () -> {
-            System.out.println("Task 3 started");
-            System.out.println("Task 3 completed");
-        };
-        
-        Thread t1 = new Thread(job1);
-        Thread t2 = new Thread(job2);
-        Thread t3 = new Thread(job3);
-        
-        t1.start();
-        t2.start();
-        t3.start();
-        
-        System.out.println("All tasks started");
+        task(1);
+        task(2);
+        task(3);
+        System.out.println("All tasks done");
     }
 }
--- Functional_Interface_1/Functional Interfaces/TemperatureAlertSystem.java
+++ Functional_Interface_1/Functional Interfaces/TemperatureAlertSystem.java
@@ -1,30 +1,13 @@
-import java.util.function.Predicate;
-
 public class TemperatureAlertSystem {
-    
+    static boolean isHot(double t) { return t > 35.0; }
+
     public static void main(String[] args) {
-        Predicate<Double> isHot = temp -> temp > 35.0;
-        
         double temp1 = 25.0;
         double temp2 = 40.0;
         double temp3 = 33.0;
-        
-        if (isHot.test(temp1)) {
-            System.out.println("Temperature " + temp1 + " is too hot!");
-        } else {
-            System.out.println("Temperature " + temp1 + " is normal");
-        }
-        
-        if (isHot.test(temp2)) {
-            System.out.println("Temperature " + temp2 + " is too hot!");
-        } else {
-            System.out.println("Temperature " + temp2 + " is normal");
-        }
-        
-        if (isHot.test(temp3)) {
-            System.out.println("Temperature " + temp3 + " is too hot!");
-        } else {
-            System.out.println("Temperature " + temp3 + " is normal");
-        }
+
+        System.out.println("Temperature " + temp1 + (isHot(temp1) ? " is too hot!" : " is normal"));
+        System.out.println("Temperature " + temp2 + (isHot(temp2) ? " is too hot!" : " is normal"));
+        System.out.println("Temperature " + temp3 + (isHot(temp3) ? " is too hot!" : " is normal"));
     }
 }
